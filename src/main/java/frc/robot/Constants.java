85d2f7ecc9ed22ef4877d5815e1d100cb19320a8
Victor Tan <shenlain.nyc@gmail.com>
2024-03-31 15:03:31 -0400

    Init

---
+++ src/main/java/frc/robot/Constants.java
@@ -0,0 +1,7 @@
+package frc.robot;
+
+public class Constants {
+    public final static String CANBUS = "rio";
+    public final static int LEADER_TALON_FX_ID = 4;
+    public final static int FOLLOWER_TALON_FX_ID = 5;
+}
